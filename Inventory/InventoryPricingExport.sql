f2f2bc1f33cd606998606398b27a3e43fac8000b
Steven Rolfe <steve.rolfe@gmail.com>
2013-10-09 15:06:27 -0400

    Added Inventory Pricing

---
+++ Inventory/InventoryPricingExport.sql
@@ -0,0 +1,23 @@
+/*
+
+ +-------------------------+
+ | MOM Item Pricing Export | MOM v6.2 -> CB 13.2
+ +-------------------------+---------------------------------------+
+ | Sanitizes and translates between MOM and CB database structures |
+ +-----------------------------------------------------------------+
+
+ */
+
+
+CurrencyCode
+ItemCode
+PricingMethod
+StandardCost
+PricingCost
+WholesalePrice
+RetailPrice
+SuggestedRetailPrice
+WholeSalePercentAmount
+RetailPercentAmount
+WholesalesMaxDiscount
+RetailMaxDiscount
