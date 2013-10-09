b42ff8542fb85e47dd512106bd925270704f5f78
Steven Rolfe <steve.rolfe@gmail.com>
2013-10-09 15:34:35 -0400

    Standard Inventory Pricing Finalized

--- Inventory/InventoryPricingExport.sql
+++ Inventory/InventoryPricingExport.sql
@@ -8,16 +8,27 @@
 
  */
 
-
-CurrencyCode
-ItemCode
-PricingMethod
-StandardCost
-PricingCost
-WholesalePrice
-RetailPrice
-SuggestedRetailPrice
-WholeSalePercentAmount
-RetailPercentAmount
-WholesalesMaxDiscount
-RetailMaxDiscount
+SELECT
+	'USD' AS 'CurrencyCode',
+	
+	(
+		SELECT
+			i.ItemCode
+		FROM
+			[CBERP\CBERPSQL].acdd.dbo.InventoryItem i
+		WHERE
+			i.ItemName = rtrim(ltrim(s.NUMBER))
+	) AS 'ItemCode',
+	
+	'None' AS 'PricingMethod',
+	s.UNCOST AS 'StandardCost',
+	s.UNCOST AS 'PricingCost',
+	s.PRICE1 AS 'WholesalePrice',
+	s.PRICE1 AS 'RetailPrice',
+	0.00 AS 'SuggestedRetailPrice',
+	0.00 AS 'WholeSalePercentAmount',
+	0.00 AS 'RetailPercentAmount',
+	0.00 AS 'WholesalesMaxDiscount',
+	0.00 AS 'RetailMaxDiscount'
+FROM
+	[MailOrderManager].[dbo].[STOCK] s
