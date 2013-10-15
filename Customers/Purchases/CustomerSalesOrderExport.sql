fa4319fc5b197eaf166136a6fe2958b9afd7242b
Steven Rolfe <steve.rolfe@gmail.com>
2013-10-15 11:33:01 -0400

    Sales order export nearing completion

--- Customers/Purchases/CustomerSalesOrderExport.sql
+++ Customers/Purchases/CustomerSalesOrderExport.sql
@@ -8,37 +8,6 @@
 
  */
 
-ORDER_TYPE
-
-ABF - Freight
-DCN - Recip shipping No
-DFR - Free Ship UPS
-DU2 - UPS 2nd day air
-DU3 - UPS 3 day
-DUN - UPs next day
-DUP - UPS GRD
-FG  - Fedex Gnd Comm
-FH  - Fedex Commercial
-FRS - Free ship UPS GND
-FRT - Freight
-FX2 - Fedex Express 2 day air
-FX3 - Fedex Express 3 day saver
-FXG - Fedex Ground
-FXP - Fedex Priority Overnight
-FXS - Fedex Standard Overnight
-OTH - Other
-PKU - Customer pickup
-UFT - UPS Freight
-UP2 - UPS Second Day Air
-UP3 - UPS 3 day select
-UPC - UPS Ground - comm
-UPN - UPS Nxt day air
-UPS - UPS Ground
-
-CUSTNUM BILL
-SHIPNUM
-SOLDNUM (hardly used)
-
 SELECT
 	CASE ltrim(rtrim(s.PAYMETHOD))
 		WHEN 'CC' THEN 'Credit Card'
@@ -74,91 +43,129 @@ SELECT
 		END AS 'ShippingMethodCode',
 	
 	'DEFAULT' AS 'ShippingMethodGroup',
-	ShipToCode
+	
+	/* Will be generated */
+	NULL AS 'ShipToCode',
+	NULL AS 'BillToCode',
 	NULL AS 'SalesOrderCode',
-	BillToCode
+	NULL AS 'ContactCode',
+	
 	s.SHIP_DATE AS 'ShippingDate',
 	s.SHIP_DATE AS 'LatestShipDate',
-	ExchangeRate
+	1.0 AS 'ExchangeRate',
 	'Sales Order' AS 'Type',
 	'Sales' AS 'ApplyTo',
+	
 ARAccountCode
-	BillToCountry
-	BillToPostalCode
-	ContactCode
-	CouponCode
-	CouponType
 DiscountAccountCode
-	NULL AS 'DiscountType',
 FreightAccountCode
-	FreightTaxCode
-	OpportunityCode
 OtherAccountCode
-	OtherTaxCode
+
+	NULL AS 'DiscountType',
+	'Sales No Tax' AS 'FreightTaxCode',
+	NULL AS 'OpportunityCode',
+
+	'Sales No Tax' AS 'OtherTaxCode',
 	PaymentTermGroup
-	ReceivableCode
-	ShipToCountry
-	ShipToName
-	ShipToPostalCode
-	SourceCode
+	NULL AS 'ReceivableCode',
+	
+	CASE ltrim(rtrim(s.ORDERTYPE))
+		WHEN 'RETPHO' THEN 'Phone'
+		WHEN 'NEWPHO' THEN 'Phone'
+		WHEN 'RETWEB' THEN 'Web'
+		WHEN 'NEWWEB' THEN 'Web'
+		ELSE 'Unknown'
+		END AS 'SourceCode',
+	
 	'MAIN' AS 'WarehouseCode',
 	NULL AS 'SourceSalesOrderCode',
 	NULL AS 'RootDocumentCode',
 	s.ORD_DATE AS 'SalesOrderDate',
 	NULL AS 'ProjectCode',
-	BillToName
 	s.ORD_TOTAL - (s.OTHERCOST + s.SHIPPING + s.TAX) AS 'SubTotal',
 	s.ORD_TOTAL - (s.OTHERCOST + s.SHIPPING + s.TAX) AS 'SubTotalRate',
 	s.SHIPPING AS 'Freight',
-	FreightRate
-	FreightTax
-	FreightTaxRate
+	s.SHIPPING AS 'FreightRate',
+	0.00 AS 'FreightTax',
+	0.00 AS 'FreightTaxRate',
 	s.TAX AS 'Tax',
 	s.TAX AS 'TaxRate',
 	s.ORD_TOTAL AS 'Total',
 	s.ORD_TOTAL AS 'TotalRate',
 	s.PONUMBER AS 'POCode',
-	SalesRepOrderCode
-	PODate
-	DueDate
-	CancelDate
-	AmountPaid
-	AmountPaidRate
-	IsPaid
+	NULL AS 'SalesRepOrderCode',
+	s.ORD_DATE AS 'PODate',
+	dateadd(day, s.DUE_DAYS, s.ORD_DATE) AS 'DueDate',
+	NULL AS 'CancelDate',
+	s.CHECKAMOUN AS 'AmountPaid',
+	s.CHECKAMOUN AS 'AmountPaidRate',
+	
+	CASE WHEN s.ORD_TOTAL <= s.CHECKAMOUN
+		THEN 1
+		ELSE 0
+		END AS 'IsPaid',
+	
 	s.DUEDAYS AS 'DaysBeforeInterest',
-	DiscountableDays
-	DiscountPercent
-	InterestPercent
-	DueType
-	StartDate
-	DatePaid
-	OrderStatus
+	0 AS 'DiscountableDays',
+	0 AS 'DiscountPercent',
+	0 AS 'InterestPercent',
+	'Net Days - From Invoice Date' AS 'DueType',
+	NULL AS 'StartDate',
+	NULL AS 'DatePaid',
+	
+	CASE s.ORD_ST2
+		WHEN 'BI' THEN 'Open' /* Ready to invoice */
+		WHEN 'BO' THEN 'Open' /* Back-ordered */
+		WHEN 'CD' THEN 'Voided' /* Credit denied - Suspended */
+		WHEN 'CN' THEN 'Voided' /* Canceled */
+		WHEN 'FH' THEN 'Open' /* Fraud hold */
+		WHEN 'GD' THEN 'Voided' /* Google Checkout - Canceled */
+		WHEN 'GN' THEN 'Open' /* Google Checkout - Ready to charge */
+		WHEN 'GS' THEN 'Completed' /* Google Checkout - Shipped */
+		WHEN 'II' THEN 'Voided' /* Invalid credit info - Suspended */
+		WHEN 'IN' THEN 'Open' /* Invoiced - Ready to pack */
+		WHEN 'OR' THEN 'Open' /* Order on review */
+		WHEN 'PE' THEN 'Open' /* Permanent hold */
+		WHEN 'QO' THEN 'Open' /* Quotation */
+		WHEN 'SH' THEN 'Completed' /* Shipped all merchandise */
+		WHEN 'UO' THEN 'Open' /* Uncompleted - Order on hold */
+		ELSE 'Open'
+		END AS 'OrderStatus',
+	
+	BillToName
 	BillToAddress
 	BillToCity
 	BillToState
 	BillToCounty
 	BillToPhone
 	BillToPhoneExtension
+	BillToCountry
+	BillToPostalCode
+	
+	ShipToCountry
+	ShipToName
+	ShipToPostalCode
 	ShipToAddress
 	ShipToCity
 	ShipToState
 	ShipToCounty
 	ShipToPhone
 	ShipToPhoneExtension
+	
 	'USD' AS 'CurrencyCode',
 	s.PROCSSD AS 'IsProcessed',
 	1 AS 'IsAllowBackOrder',
-	Balance
-	BalanceRate
-	AppliedCredit
-	AppliedCreditRate
-	Discount
-	DiscountRate
+	(s.ORD_TOTAL - s.CHECKAMOUN) AS 'Balance',
+	(s.ORD_TOTAL - s.CHECKAMOUN) AS 'BalanceRate',
+	0.00 AS 'AppliedCredit',
+	0.00 AS 'AppliedCreditRate',
+	0.00 AS 'Discount',
+	0.00 AS 'DiscountRate',
 	0.00 AS 'WriteOff',
 	0.00 AS 'WriteOffRate',
 	s.OTHERCOST AS 'Other',
 	s.OTHERCOST AS 'OtherRate',
-	OtherName
+	NULL AS 'OtherName',
 	0.00 AS 'OtherTax',
 	'Sales No Tax' AS 'OtherTaxRate',
 	
@@ -171,39 +178,99 @@ OtherAccountCode
 			ltrim(rtrim(o.DESC3)) + CHAR(13) + CHAR(10) + 
 			ltrim(rtrim(o.DESC4)) + CHAR(13) + CHAR(10) + 
 			ltrim(rtrim(o.DESC5)) + CHAR(13) + CHAR(10) + 
-			ltrim(rtrim(o.DESC6)) + CHAR(13) + CHAR(10)
+			ltrim(rtrim(o.DESC6))
 		FROM
 			[MailOrderManager].[dbo].[ORDMEMO] o
 		WHERE
 			o.ORDERNO = s.ORDERNO
 	) AS 'Notes',
 	
-	IsVoided
+	CASE s.ORD_ST2
+		WHEN 'CD' THEN 1 /* Credit denied - Suspended */
+		WHEN 'CN' THEN 1 /* Canceled */
+		WHEN 'GD' THEN 1 /* Google Checkout - Canceled */
+		WHEN 'II' THEN 1 /* Invalid credit info - Suspended */
+		ELSE 0
+		END AS 'IsVoided',
+	
 	IsOnHold
 	IsPrinted
-	IsOrderAcknowledged
-	IsFromContract
-	NULL AS 'RecurDocumentCode',
 	PrintCount
-	CouponID
-	CouponDiscount
-	CouponDiscountRate
-	CouponDiscountType
-	CouponDiscountPercent
-	CouponDiscountAmount
-	CouponUsage
-	CouponDiscountIncludesFreeShipping
-	CouponRequiresMinimumOrderAmount
+	IsOrderAcknowledged
+	0 AS 'IsFromContract',
+	NULL AS 'RecurDocumentCode',
+	
+	(
+		SELECT
+			ltrim(rtrim(c.PROMO_CODE))
+		FROM
+			[MailOrderManager].[dbo].[ORDTOOLS] c
+		WHERE
+			c.ORDERNO = s.ORDERNO
+	) AS 'CouponCode',
+	
+	CouponType
+	
+	(
+		SELECT
+			ltrim(rtrim(c.PROMO_CODE))
+		FROM
+			[MailOrderManager].[dbo].[ORDTOOLS] c
+		WHERE
+			c.ORDERNO = s.ORDERNO
+	) AS 'CouponID',
+	
+	(
+		SELECT
+			c.AMOUNT
+		FROM
+			[MailOrderManager].[dbo].[ORDTOOLS] c
+		WHERE
+			c.ORDERNO = s.ORDERNO
+	) AS 'CouponDiscount',
+	
+	(
+		SELECT
+			c.AMOUNT
+		FROM
+			[MailOrderManager].[dbo].[ORDTOOLS] c
+		WHERE
+			c.ORDERNO = s.ORDERNO
+	) AS 'CouponDiscountRate',
+	
+	'Amount' AS 'CouponDiscountType',
+	NULL AS 'CouponDiscountPercent',
+	NULL AS 'CouponDiscountAmount',	
+	~s.NO_PROMO AS 'CouponUsage',
+	
+	(
+		SELECT
+			CASE WHEN ltrim(rtrim(c.SHIP_VIA)) IS NOT NULL
+				THEN 1
+				ELSE 0
+				END
+		FROM
+			[MailOrderManager].[dbo].[ORDTOOLS] c
+		WHERE
+			c.ORDERNO = s.ORDERNO
+	) AS 'CouponDiscountIncludesFreeShipping',
+	
+	0 AS 'CouponRequiresMinimumOrderAmount',
 	IsFreightOverwrite
-	WebSiteCode
+	NULL AS 'WebSiteCode',
 	NULL AS 'WaveCode',
-	SourceType
+	NULL AS 'SourceType',
 	NULL AS 'POSWorkstationID',
 	NULL AS 'POSClerkID',
 	NULL AS 'DownloadEmailSentDate',
-	RootOrderCode
-	DateShipped
-	IsShipped
+	NULL AS 'RootOrderCode',
+	s.SHIP_DATE AS 'DateShipped',
+	
+	CASE WHEN s.SHIP_DATE IS NOT NULL
+		THEN 1
+		ELSE 0
+		END AS 'IsShipped',
+		
 	NULL AS 'SaveCounterID',
 	NULL AS 'CouponComputation',
 	NULL AS 'TaxGroup1',
@@ -227,10 +294,27 @@ OtherAccountCode
 	NULL AS 'StoreMerchantID_DEV000221',
 	NULL AS 'PublicNotesDescription',
 	NULL AS 'PublicNotes',
-	ReturnedSubTotal
-	ReturnedSubTotalRate
-	ReturnedTax
-	ReturnedTaxRate
+	
+	(
+		SELECT
+			r.RMA_TOTAL
+		FROM
+			[MailOrderManager].[dbo].[RMA] r
+		WHERE
+			r.ORDERNO = s.ORDERNO
+	) AS 'ReturnedSubTotal',
+	
+	(
+		SELECT
+			r.RMA_TOTAL
+		FROM
+			[MailOrderManager].[dbo].[RMA] r
+		WHERE
+			r.ORDERNO = s.ORDERNO
+	) AS 'ReturnedSubTotalRate',
+	
+	0.00 AS 'ReturnedTax',
+	0.00 AS 'ReturnedTaxRate',
 	
 	CASE WHEN ltrim(rtrim(s.SHIPLIST)) = 'PKU'
 		THEN 1
@@ -239,12 +323,18 @@ OtherAccountCode
 		
 	NULL AS 'BillToAddressType',
 	NULL AS 'ShipToAddressType',
-	WarehouseAddressType
+	NULL AS 'WarehouseAddressType',
 	NULL AS 'IsBlindShip',
 	IsFreightQuoted
-	Signature
-	SignatureSVG
+	NULL AS 'Signature',
+	NULL AS 'SignatureSVG',
 	NULL AS 'CBNMasterID',
 	NULL AS 'BillToPlus4',
 	NULL AS 'ShipToPlus4'
+FROM
+	[MailOrderManager].[dbo].[CMS] s
+	JOIN [MailOrderManager].[dbo].[CUST] b
+		ON b.CUSTNUM = s.CUSTNUM
+	JOIN [MailOrderManager].[dbo].[CUST] h
+		ON h.CUSTNUM = (CASE WHEN s.SHIPNUM = 0 THEN s.CUSTNUM ELSE s.SHIPNUM END)
 WHERE s.QUOTATION = 0
